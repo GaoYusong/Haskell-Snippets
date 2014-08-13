96dbd6cd89b6552036650483f83687d9e5abe282
GaoYusong <yusong.gao@gmail.com>
2014-08-13 20:25:05 +0800

    some work for GrahamScan 2

--- GrahamScan.hs
+++ GrahamScan.hs
@@ -7,14 +7,28 @@ data Vector = Vector Integer Integer
             deriving (Show)
                      
 data Direction = LEFT | RIGHT | STRAIGHT
-               deriving (Show)
+               deriving (Eq, Show)
 
 grahamScan :: [Point2D] -> [Point2D]
 grahamScan points =
   let 
     sortedPoints = Data.List.sortBy pointCompare points
   in
-   sortedPoints
+   if length sortedPoints >= 3 then
+     grahamScanImpl (reverse (take 3 sortedPoints)) (drop 3 sortedPoints)
+   else
+     sortedPoints
+
+grahamScanImpl :: [Point2D] -> [Point2D] -> [Point2D]
+grahamScanImpl (p2:p1:stacks) (p:points)
+  | direction p1 p2 p /= LEFT =
+      grahamScanImpl (p1:stacks) (p:points)
+  | otherwise =
+      grahamScanImpl (p:p2:p1:stacks) points
+grahamScanImpl stacks (p:points) =
+  grahamScanImpl (p:stacks) points
+grahamScanImpl stacks [] =
+  stacks
 
 directions :: [Point2D] -> [Direction]
 directions (a:b:c:points) = (direction a b c) : (directions (b:c:points))
